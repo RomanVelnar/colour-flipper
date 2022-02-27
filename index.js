17953f54db7fddcdf6dc5dbf8d0324d96fdb08a0
RomanVelnar <roman.velnar@gmail.com>
2022-02-27 19:55:12 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
+const btn = document.getElementById("btn")
+const colour = document.querySelector(".colour")
+
+btn.addEventListener("click", function() {
+    let hexColour = "#"
+    for(let i = 0; i < 6; i++) {
+        hexColour += hex[getRandomNumber()]
+    }
+    colour.textContent = hexColour
+    document.body.style.backgroundColor = hexColour
+})
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length)
+} 
